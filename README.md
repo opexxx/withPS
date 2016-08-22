9eb4c192a63b59c01533bb5aeb1abac8467cec86
Acader <Acader@users.noreply.github.com>
2016-08-22 16:23:33 +0200

    More code cleanup, fixed Readme - Added better argument handling, in "with node -v" the -v will no longer be ignored - Added command check for "!command" - Changed "command:" to "command>" - Changed Write-Output to Write-Host - Changed function name to the PowerShell Verb-Noun format - Changed Invoke-WebRequest to Net.WebClient DownloadFile - Removed unnecessary install checks - Removed install hint - Removed prog prefix of some vars - Removed unnecessary code from with.cmd - Changed version to 0.0.4

--- README.md
+++ README.md
@@ -7,18 +7,20 @@ Program prefixing for continuous workflow using a single tool
 Usable with PowerShell and CMD
 
 ### Installation
-#### From PowerShell
-1. Open Powershell
-2. Enter: `(Invoke-WebRequest https://raw.githubusercontent.com/Acader/withPS/master/withps.ps1).content | iex`
-3. Restart your comupter.
+```
+(New-Object Net.WebClient).DownloadString("https://cdn.rawgit.com/Acader/withPS/master/withps.ps1") | iex
 
-#### Or the old way
-1. Clone this repository
-2. Run `.\withps.ps1 -i`
-3. Restart your comupter
+```
+or
+```
+(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Acader/withPS/master/withps.ps1") | iex
+```
+or
+```
+.\withps.ps1 -i
+```
 
-
-
+ 
 ### Usage
 
 `with <program>`
@@ -30,9 +32,9 @@ See `with -h` for mor help
 For example:
 ```
 $ with git
-git: add .
-git: commit -a -m "Commited"
-git: push
+git> add .
+git> commit -a -m "Commited"
+git> push
 ```
 
 
@@ -48,29 +50,29 @@ java Primes> 4
 And to repeat commands:
 ```
 $ with gcc -o output input.c
-gcc -o -output input.c:
+gcc -o -output input.c>
 <enter>
 Compiling...
-gcc -o -output input.c:
+gcc -o -output input.c>
 ```
 
 
 To execute a shell command proper prefix line with `:` or `!`
 
 
-`git: :dir` or `git: !dir`
+`git> :dir` or `git> !dir`
 
 You can also drop and add different commands.
 
 ```
 with git
-git: +add
-git add: <some file>
-git add: -
-git: +add test.txt
-git add test.txt: --
-git:
+git> +add
+git> add: <some file>
+git> add: -
+git> +add test.txt
+git> add test.txt: --
+git>
 ```
 
-To exit use `:exit` or press Ctrl + C.
+To exit enter `:exit` or press Ctrl + C.
 
--- withps.ps1
+++ withps.ps1
@@ -1,84 +1,86 @@
-param (
-    [switch]$help = $false,
-    [switch]$version = $false,
-    [switch]$update = $false,
-    [switch]$install = $false,
-    [switch]$remove = $false
-)
-$progVersion = "0.0.3"
-$progInstallFolder = "$env:APPDATA\withps"
-$progPath = $MyInvocation.MyCommand.Path
-$progInstalled = $false
-if (-not $progPath) {
-    $install = $true
-} elseif ($progPath.StartsWith($progInstallFolder)) {
-    $progInstalled = $true
-}
-$program = $args -join " "
+$version = "0.0.4"
+$installFolder = "$env:APPDATA\withps"
 
-function printHelp {
-    Write-Output "Syntax: with [-h] [-v] [-u] [-i] [-r] program"
-    Write-Output "-h, -help:    Display this message"
-    Write-Output "-v, -version: Display the currently installed version of with"
-    Write-Output "-u, -update:  Updates with"
-    if (-Not $progInstalled) {
-        Write-Output "-i, -install: Installs withPS in PATH"
-    } else {
-        Write-Output "-r, -remove:  Completely removes with form the Computer"
-    }    
+function Print-Help {
+    Write-Host "Syntax: with program"
+    Write-Host "-h, -help:    Display this message"
+    Write-Host "-v, -version: Display the currently installed version of with"
+    Write-Host "-u, -update:  Updates with"
+    Write-Host "-i, -install: Installs withPS in PATH"
+    Write-Host "-r, -remove:  Completely removes with form the Computer"
+    Write-Host "-g, -github:  Opens the withPS GitHub page"
 }
 
-function printVersion {
-    Write-Output "PowerShell with $progVersion"
+function Print-Version {
+    Write-Host "PowerShell with $version"
 }
 
 function update {
-    Write-Output "Updating withPS $progVersion"
-    Invoke-WebRequest https://raw.githubusercontent.com/Acader/withPS/master/withps.ps1 -OutFile "$progInstallFolder\withps.ps1"
-    Invoke-WebRequest https://raw.githubusercontent.com/Acader/withPS/master/with.cmd -OutFile "$progInstallFolder\with.cmd"
+    if (Test-Path $installFolder) {
+        Write-Host "Updating withPS $version"
+        (New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/Acader/withPS/master/withps.ps1", "$installFolder\withps.ps1")
+        (New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/Acader/withPS/master/with.cmd", "$installFolder\with.cmd")
+    } else {
+        Write-Warning "with is not installed. To install it run 'with -i'"
+    }
 }
 
 function install {
-    Write-Output "Installing withPS ..."
-    if (-not (Test-Path $progInstallFolder)) {
-        New-Item $progInstallFolder -Force -itemtype directory | Out-Null    
+    Write-Host "Installing withPS ..."
+    if (-not (Test-Path $installFolder)) {
+        New-Item $installFolder -Force -itemtype directory | Out-Null
     }
     $oldPath = [Environment]::GetEnvironmentVariable("PATH", "User")
-    if ($oldPath -notlike "*$progInstallFolder*") {        
-        $newPath = "$oldPath;$progInstallFolder"
+    if ($oldPath -notlike "*$installFolder*") {        
+        $newPath = "$oldPath;$installFolder"
         [Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
     }
     update
-    Write-Output "Installation finished"
+    Write-Host "Installation finished"
 }
 
 function remove {
-    Write-Output "Removing withPS ..."
+    Write-Host "Removing withPS ..."
     $oldPath = [Environment]::GetEnvironmentVariable("PATH", "User")
-    if ($oldPath -like "*$progInstallFolder*") {        
-        $newPath = $oldPath.Replace(";$progInstallFolder", "")
+    if ($oldPath -like "*$installFolder*") {        
+        $newPath = $oldPath.Replace(";$installFolder", "")
         [Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
     }
-    If (Test-Path $progInstallFolder){
-        Remove-Item -Recurse -Force $progInstallFolder
+    If (Test-Path $installFolder){
+        Remove-Item -Recurse -Force $installFolder
     }
 
-    Write-Output "withPS is now compleatly removed from you System"
+    Write-Host "withPS is now compleatly removed from you System"
 
 }
 
+function Test-Command ($com) {
+    if (-not $com) {
+        return $false
+    }
+    $progName = ($com -split " ")[0]
+    if (Get-Command $progName -errorAction SilentlyContinue) {
+        return $true
+    }
+    Write-Warning "'$progName' is not valid command"
+    return $false
+}
+
 function run {
-    Write-Host -NoNewline "$program`: "
+    Write-Host -NoNewline "$program`> "
     $command = Read-Host
     switch -wildcard ($command) {
         "[:!]*" {
-            Invoke-Expression $command.Substring(1)
+            $command = $command.Substring(1)
+            if (Test-Command $command) {
+                Invoke-Expression $command
+            }
         }
         "+*" {
             $temp = $command.Substring(1)
             if ($temp.Length -gt 0) {
                 $program = "$program " + $temp    
-            }        
+            }
         }
         "-" {
             $temp = ($program -split " ")
@@ -96,25 +98,26 @@ function run {
     run
 }
 
-if (-not ($progInstalled -or $install)) {
-    Write-Output "It seams 'with' is not installed run 'with -i' to install it"
-}
-
-if ($install -and -not $progInstalled) {
+if (-not $MyInvocation.MyCommand.Path) {
     install
-} elseif ($update -and $progInstalled) {
-    update
-} elseif ($remove -and $progInstalled) {
-    remove
-} elseif ($version) {
-    printVersion
-} elseif ($help) {
-    printHelp
-} elseif ($program) {
-    $programName = ($program -split " ")[0]
-    if (Get-Command $programName -errorAction SilentlyContinue) {
-        run
-    } else {
-        Write-Output "error: '$programName' is not installed"
+    exit
+}
+$program = $args -join " "
+
+switch -wildcard ($args[0]) {
+    "-h*" { Print-Help }
+    "-v*" { Print-Version }
+    "-u*" { update }
+    "-i*" { install }
+    "-r*" { remove }
+    "-g*" { Start-Process "https://github.com/Acader/withPS" }
+    Default {
+        if ($program) {
+            if(Test-Command $program) {
+                run
+            } 
+        } else {
+            Print-Help
+        }
     }
 }
